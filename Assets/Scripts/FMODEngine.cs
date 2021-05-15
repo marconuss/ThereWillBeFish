eebb2277bcb18edaf0afd9f61512ae08be82a453
marconuss <marconuss95@gmail.com>
2021-05-15 17:40:09 +0200

    Fmod engine fix...hopefully

--- Assets/Scripts/FMODEngine.cs
+++ Assets/Scripts/FMODEngine.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class FMODEngine : MonoBehaviour
 {
-    [SerializeField]
-    private FMODUnity.StudioEventEmitter emitter;
+    private FMOD.Studio.EventInstance eventInstance;
+
+    [FMODUnity.EventRef]
+    public string fmodEvent;
+
+    //[SerializeField] [Range(-5f, 5f)]
+    //private float pitchToHeight = 0.0f;
+    //
+    //[SerializeField] [Range(0.5f, 2f)]
+    //private float pitchToVelocity = 1.0f;
+
 
     private void Start()
     {
-        emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+        eventInstance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        eventInstance.start();
+
     }
-    private void FixedUpdate()
+
+    private void Update()
     {
-        emitter.SetParameter("PitchToHeight", Submarine.Instance.transform.position.y);
-        emitter.SetParameter("PitchToVelocity", Submarine.Instance.velocity.x);
+
+        //eventInstance.setParameterByName("PitchToHeight", pitchToHeight);
+        //eventInstance.setParameterByName("PitchToVelocity", pitchToVelocity);
+
+        eventInstance.setParameterByName("PitchToHeight", Submarine.Instance.transform.position.y);
+        eventInstance.setParameterByName("PitchToVelocity", Submarine.Instance.velocity.x);
     }
 }
